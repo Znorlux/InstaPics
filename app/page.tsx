d0d97ccd5406bf9ae3ef6470af8641c3d55aff7f
Znorlux <mabe0898@hotmail.com>
2024-03-28 22:40:48 -0500

    Implementacion de barra de progreso y nueva vista de dropzone

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 import { useState } from "react";
 import { useEdgeStore } from "@/lib/edgestore";
 import Link from "next/link";
-
+import { SingleImageDropzone } from "./components/single-image-dropzone";
 export default function Home() {
-  const [file, setFile] = useState<File | null>(null);
+  const [file, setFile] = useState<File>();
+  const [progress, setProgress] = useState(0);
   const { edgestore } = useEdgeStore();
   const [urls, setUrls] = useState<{
     url: string;
@@ -14,17 +15,33 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center m-6 gap-2">
-      <input
-        type="file"
-        onChange={(e) => {
-          setFile(e.target.files?.[0] ?? null);
+      <SingleImageDropzone
+        width={200}
+        height={200}
+        value={file}
+        dropzoneOptions={{ maxSize: 1024 * 1024 * 15 }} // Peso maximo 15 MB
+        onChange={(file) => {
+          setFile(file);
         }}
       />
+      <div className="h-[6px] w-44 border rounded overflow-hidden mt-2">
+        <div
+          className="h-full bg-white transition-all durantion-150"
+          style={{
+            width: `${progress}%`,
+          }}
+        />
+      </div>
       <button
         className="bg-white text-black rounded px-2 hover:opacity-80"
         onClick={async () => {
           if (file) {
-            const res = await edgestore.myPublicImages.upload({ file });
+            const res = await edgestore.myPublicImages.upload({
+              file,
+              onProgressChange: (progress) => {
+                setProgress(progress);
+              },
+            });
             //Guardamos la url y la url de la miniatura
             setUrls({
               url: res.url,
@@ -33,7 +50,7 @@ export default function Home() {
           }
         }}
       >
-        Upload
+        Subir
       </button>
       {urls?.url && (
         <Link href={urls.url} target="_blank">
